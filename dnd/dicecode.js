1458bf7ee41dda0e39934c4311ac4777302f09de
BethMI <w0239326@nscc.ca>
2019-03-16 23:22:18 -0300

    added dnd dice game

---
+++ dnd/dicecode.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", rollout);
+
+function rollout() {
+    let dieSelect = document.querySelector("#chooseDie");
+    let diceInput = document.querySelector("#numberOfDice");
+    let rollBtn = document.querySelector("#roll");
+    let diceMax = parseInt(dieSelect.value);
+    let diceArray = [];
+    let totalRolled = 0;
+    let diceNumber;
+
+    dieSelect.addEventListener("change", getMax);
+    rollBtn.addEventListener("click", rollDice);
+
+    function getMax(){
+        diceMax = parseInt(dieSelect.value);
+        // console.log(diceMax);
+    }
+
+    function rollDice(){
+        let diceSection = document.querySelector("#diceDisplay > div");
+        totalRolled = 0;
+        diceSection.innerHTML = "";
+
+        diceNumber = diceInput.value;
+
+        for (let counter = 0; counter < diceNumber; counter++){
+
+            rollValue = Math.floor(Math.random() * diceMax + 1);
+            totalRolled += rollValue;
+
+            let die = document.createElement("output");
+            die.setAttribute("class", "die");
+            die.value = rollValue;
+
+            diceArray.push(die);//save this for later functionality
+
+            diceSection.appendChild(die);
+
+        }
+
+        document.querySelector("#total").value = totalRolled;
+
+    }
+
+
+
+}
